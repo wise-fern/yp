5a126aa056994630b565f03ce3a17e8a4afac46c
wise-fern <wise.fern2345@fastmail.com>
2026-04-02 07:18:41 -0400

    add implementation

--- libyunpa/src/Graphics/Elements/Text.cpp
+++ libyunpa/src/Graphics/Elements/Text.cpp
@@ -1,3 +1,26 @@
 module;
-
+#include <string>
+#include <string_view>
 module libyunpa;
+import :Text;
+
+namespace libyunpa {
+Text::Text(const Options &options)
+    : Positionable(options), _text(options.text) {
+  update();
+  height(1);
+}
+
+void Text::update() {
+  output(Positionable::apply(_text));
+  width(_text.size());
+}
+
+void Text::set_text(std::string_view text) {
+  _text = text;
+}
+
+std::string_view Text::get_text() const {
+  return _text;
+}
+} // namespace libyunpa
