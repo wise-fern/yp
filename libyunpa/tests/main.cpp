a852bf18751af9219de2efaf628f50bdffcd0423
wise-fern <wise.fern2345@fastmail.com>
2026-04-02 08:48:00 -0400

    update tester

--- libyunpa/tests/main.cpp
+++ libyunpa/tests/main.cpp
@@ -2,7 +2,13 @@ import libyunpa;
 #include <iostream>
 
 int main() {
-  std::cout << "\x1b[?1049h\x1b[1;1H";
+  using namespace libyunpa;
+  Text::Options options{};
+  options.position = {.x = 3, .y = 4};
+  options.text = "Hello, world!";
+  Text text{options};
+  std::cout << "\x1b[?1049h\x1b[1;1HAlternate Buffer";
+  text.draw();
   std::string input;
   std::cin >> input;
   std::cout << "\x1b[?1049l";
