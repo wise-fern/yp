f4e40103843866d175fb91aa88a96458bdf72c48
wise-fern <wise.fern2345@fastmail.com>
2026-04-05 13:05:11 -0400

    update tester

--- libyunpa/tests/main.cpp
+++ libyunpa/tests/main.cpp
@@ -8,7 +8,9 @@ int main() {
   options.text = "Hello, world!";
   Text text{options};
   options.position = {.x = 3, .y = 4};
-  options.inverted = true;
+  options.color = Color::BLUE;
+  options.bgColor = Color::RED;
+  options.bold = true;
   Text text2{options};
   std::cout << "\x1b[?1049h\x1b[1;1HAlternate Buffer";
   text.draw();
